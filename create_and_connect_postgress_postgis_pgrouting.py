51aeaa4a207863e4b459214e495fb1b5bb80546a
JTimmermans <jtjoris@gmail.com>
2018-04-11 15:18:59 +0200

    Createed several basic postgress postigs pgrouting functions

--- create_and_connect_postgress_postgis_pgrouting.py
+++ create_and_connect_postgress_postgis_pgrouting.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 import getpass
 
+# some interesting slides: http://www.postgis.us/presentations/postgis_install_guide_22.html#/11
 
-def create_postgress_db(databasename):
+def create_postgres_db(databasename):
 	db_name = databasename
 
 	#ask password
@@ -12,13 +14,56 @@ def create_postgress_db(databasename):
 	engine = create_engine('postgresql://postgres:'+str(password)+'@localhost:5432/')
 
 	#connect to starting point
-	conn = engine.connect()
+	con = engine.connect()
 
 	#enable sql
-	conn.execute('commit')
+	con.execute('commit')
 
 	#create database
-	conn.execute("CREATE DATABASE " +db_name)
+	con.execute("CREATE DATABASE " +db_name)
+
+def connect_postgres_db(db):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    user = 'postgres'
+    password = getpass.getpass()
+    host='localhost'
+    port=5432
+
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    # The return value of create_engine() is our connection object
+    con = create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = MetaData(bind=con, reflect=True)
+
+    return con, meta
 
 
-create_postgress_db('osm3')
+def create_postgis_pgrouting():
+	con.execute('CREATE EXTENSION postgis')
+	con.execute('CREATE EXTENSION hstore')
+	con.execute('CREATE EXTENSION fuzzystrmatch')
+	con.execute('CREATE EXTENSION postgis_tiger_geocoder')
+	con.execute('CREATE EXTENSION postgis_topology')
+	con.execute('CREATE EXTENSION pgrouting')
+
+	#check if working
+	result1 = con.execute('SELECT postgis_full_version()')
+	for r in result1:
+		print(r)
+
+	result2 = con.execute('SELECT * FROM pgr_version()')
+	for r in result2:
+		print(r)
+
+
+
+
+
+create_postgres_db('osm')
+con, meta = connect_postgres_db('osm')
+create_postgis_pgrouting()
