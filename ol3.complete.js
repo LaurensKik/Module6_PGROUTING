53d4b6a4888b9dfc5dd9244621dc35efb2df77b0
JTimmermans <jtjoris@gmail.com>
2018-05-01 14:22:53 +0200

    HOI BETYH

--- ol3.complete.js
+++ ol3.complete.js
@@ -1,13 +1,14 @@
 // START Matt Walker's code
 // Extent of the map in units of the projection (these match our base map)
-var extent = [-3276800, -3276800, 3276800, 3276800];
+var extent = [646.36 308975.28
+276050.82 636456.31];
 
 // Fixed resolutions to display the map at (pixels per ground unit (meters when
 // the projection is British National Grid))
 var resolutions = [1600,800,400,200,100,50,25,10,5,2.5,1,0.5,0.25,0.125,0.0625];
 
-// Define British National Grid Proj4js projection (copied from http://epsg.io/27700.js)
-proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
+// Define British National Grid Proj4js projection (copied from http://epsg.io/28992.js)
+proj4.defs(proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
 
 // Define an OL3 projection based on the included Proj4js projection
 // definition and set it's extent.
